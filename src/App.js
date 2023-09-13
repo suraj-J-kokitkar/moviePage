5ac56587dcee8fca1440daf5b08e4943741ae326
suraj-J-kokitkar <kokitkar.suraj5@gmail.com>
2023-09-13 18:24:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import movies from './components/movie.json'
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+      <div className='main'>
+        {
+          movies.map((element,index) => {
+            return(
+            <Movie key={index}
+              title={element.Title}
+              year={element.Year}
+              img={element.Poster}/>
+            )
+          })
+        }
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
